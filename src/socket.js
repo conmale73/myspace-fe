082721fb2a848bbb1e756319c03ad75969d23e9b
duchau <duchauvu0703@gmail.com>
2023-12-31 10:32:20 +0700

    fix websocket

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 const URL =
-    process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+    process.env.NODE_ENV === "production"
+        ? "https://myspace-be.onrender.com/"
+        : "http://localhost:3000";
 
 export const socket = io(URL);
